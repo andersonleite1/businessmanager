88f8c2f07ca5cc7e884665fd9c8ef3a580616793
Anderson Leite <andersonleite.dev@gmail.com>
2024-11-24 11:04:06 -0300

    chore: add file init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp /usr/local/tomcat/target/business-manager-0.0.1-SNAPSHOT.war /usr/local/tomcat/webapps/
+
+mv /usr/local/tomcat/webapps/business-manager-0.0.1-SNAPSHOT.war /usr/local/tomcat/webapps/business-manager.war
